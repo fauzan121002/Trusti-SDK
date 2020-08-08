96572f5233a60df24ad6e9d372717bd092c1a9a8
fauzan121002 <developerfauzan@gmail.com>
2020-08-08 11:18:40 +0700

    :art: new format for createAsset

--- test/createAsset.js
+++ test/createAsset.js
@@ -2,25 +2,62 @@ const trustiSDK = require('../src/index.js').trustiClient;
 const FormData = require('form-data');
 const fs = require('fs');
 
-const trusti = trustiSDK('<API - KEY>');
+const trusti = trustiSDK('324234');
 
 const newFile = fs.createReadStream(__dirname + '/SDK.txt');
 
 const form_data = new FormData();
+
 form_data.append(
   'pubkey',
-  'VEX5sYmkssvFZ3zWLYELao7mBWcCuB72kovDSDPBAFpECsMoVzW5N'
+  "VEX7dBsTJd1cQfXFR3Ts1PnreqiccpNhYsjaTdfyVmwK1duV2rnJh"
 );
+
 form_data.append('fil', newFile);
 form_data.append('asset_name', 'Test from SDK');
-form_data.append('addt_meta', '{}');
-form_data.append('multiple-signature', 0);
-form_data.append('ipfs_upload', 0);
-form_data.append('email_recipient', 'fauzanapprover1@gmail.com');
-form_data.append('approver', 'signee');
-form_data.append('addApprover', 0);
-form_data.append('add_email_sign', '[{ "value": "fauzan@gmail.com" }]'); //array in json format
-form_data.append('add_approver', '[[{ "value": "signee" }]]'); //array in json format
+
+let addt_meta = {
+  serial: "SN 12345",
+  color: "red",
+  price: "$25"
+};
+
+form_data.append('addt_meta',JSON.stringify(addt_meta));
+
+//or
+
+// form_data.append('addt_meta',`
+//   {
+//     "serial": "SN 12345",
+//     "color": "red",
+//     "price": "$25"
+//   }
+// `);
+
+
+form_data.append('multiple_signature', 1); // 0 (false) , 1 (true)
+
+let participant = [
+  {
+    email: "fauzanapprover@gmail.com",
+    status: "signer"
+  }
+]
+
+form_data.append('participant',JSON.stringify(participant));
+
+//or
+
+// form_data.append('participant',`[
+//   {
+//     "email": "fauzanapprover@gmail.com",
+//     "status": "signer"
+//   }
+// ]
+// `);
+
+form_data.append('ipfs_upload', 0); // 0 (false) , 1 (true)
+
 
 //await createAsset
 (async () => {
